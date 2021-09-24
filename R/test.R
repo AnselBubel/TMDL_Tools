dd89eb2db1c0555d9b39e660eb50b79d6db7de93
AnselBubel <ansel.bubel@gmail.com>
2021-09-24 16:52:01 -0400

    Update and rename test.r to test.R

--- R/test.R
+++ R/test.R
@@ -1,3 +1,7 @@
+##############################################
+## Testing the nutrient regression function ##
+##############################################
+
 ## Test the function with no inputs to ensure that proper error messages are given
 WQX_NUTRIENT_TMDL_Regression()
 
@@ -6,5 +10,9 @@ WQX_NUTRIENT_TMDL_Regression(SITE = "21FLPOLK_WQX-MARIANA1")
 
 ## Run the function with a test station from central Florida and a single TN regression type selected
 WQX_NUTRIENT_TMDL_Regression(SITE = "21FLPOLK_WQX-MARIANA1", TYPE="TN")
+
+## Run the function with a test station from central Florida and a single TP regression type selected
 WQX_NUTRIENT_TMDL_Regression(SITE = "21FLPOLK_WQX-MARIANA1", TYPE="TP")
+
+## Run the function with a test station from central Florida and a multiple regression type selected
 WQX_NUTRIENT_TMDL_Regression(SITE = "21FLPOLK_WQX-MARIANA1", TYPE="multiple")
